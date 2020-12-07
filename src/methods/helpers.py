loading_and_preprocessing.py - Update bug in calculation of need_by_car
helpers.py
- added another function to calculate energy from SOC

scenarios_for_users.py
- scenario 2 + 3 now also report total demand

calculate_scenarios.py
- update battery capacity to be in line with tesla powerwall

ecar_data_preprocessing.py
- car now report total charging and total demand

smaller updates for plots

src/methods/helpers.py
@@ -8,11 +8,19 @@ ureg = pint.UnitRegistry()
 
 def soc2remainingCharge(soc):
     "calculates amount of energy that is missing in the car battery"
-    charge = 0.293 * (100.0 - soc) + 0.232 * np.sqrt(100.0 - soc)  # formula to calc charge from soc
+    # charge = 0.293 * (100.0 - soc) + 0.232 * np.sqrt(100.0 - soc)  # formula to calc charge from soc
+    charge = deltasoc2tocharge(100-soc)
     assert charge >= 0.0
     # return charge * ureg.kilowatthour
     return charge
 
+def deltasoc2tocharge(delta_soc):
+    "returns energy for a given difference in state of charge"
+
+    charge = 0.293 * (delta_soc) + 0.232 * np.sqrt(delta_soc)  # formula to calc charge from soc
+    assert charge >= 0.0
+    # return charge * ureg.kilowatthour
+    return charge
 
 def remainingCharge2soc(charge):
     # wolfram alpha magic

src/methods/loading_and_preprocessing.py
@@ -2,7 +2,7 @@ import copy
 import pandas as pd
 import os
 import datetime
-from src.methods.helpers import soc2remainingCharge
+from src.methods.helpers import soc2remainingCharge, deltasoc2tocharge
 import numpy as np
 
 def get_id_matching_dict(filepath_matching):
@@ -135,9 +135,13 @@ def compute_additional_columns(car_data, drop_debug_columns=False):
 
     car_data_copy = copy.deepcopy(car_data)
 
-    needed_by_car = [soc2remainingCharge(car_data_copy["soc_start"][car_data_copy.index[i]]) -
-                     soc2remainingCharge(car_data_copy["soc_end"][car_data_copy.index[i]])
-                     for i in range(len(car_data_copy.index))]
+    try:
+        needed_by_car = car_data_copy['total_segment_charging'].apply(deltasoc2tocharge)
+
+    except KeyError:
+        needed_by_car = [soc2remainingCharge(car_data_copy["soc_start"][car_data_copy.index[i]]) -
+                         soc2remainingCharge(car_data_copy["soc_end"][car_data_copy.index[i]])
+                         for i in range(len(car_data_copy.index))]
 
     needed_by_car = np.maximum(0, needed_by_car)
     assert (np.all(np.array(needed_by_car) >= 0))